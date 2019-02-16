c7c8319f6599ec147a5e73cdb7a1ca0f7b73e920
BenHampton <BenHampton11@yahoo.com>
2019-02-16 16:37:36 -0600

    updated css

--- restaurant-ui/src/Components/Location/LocationView.js
+++ restaurant-ui/src/Components/Location/LocationView.js
@@ -3,19 +3,16 @@ import './css/locationView.css'
 
 const LocationView = props => {
     return(
-        <div>
-            <div></div>
-            <div className={'location-and-hours'}>
-                <h2 className={'location-hours-header'}>HOURS & LOCATION</h2>
-                <div className={'location-hours-text'}>
-                    <div>{props.address}</div>
-                    <div>{props.state}, {props.zipCode}</div>
-                    <div>{props.phoneNumber}</div>
-                    <br />
-                    <div>Tuesday - Friday / {props.weekDayLunchHours}</div>
-                    <div>Monday - Thursday / {props.weekendDinnerHours}</div>
-                    <div>Friday & Saturday / {props.weekendDinnerHours}</div>
-                </div>
+        <div className={'location-and-hours-container'}>
+            <h2 className={'location-hours-header'}>HOURS & LOCATION</h2>
+            <div className={'location-hours-text'}>
+                <div>{props.address}</div>
+                <div>{props.state}, {props.zipCode}</div>
+                <div>{props.phoneNumber}</div>
+                <br />
+                <div>Tuesday - Friday / {props.weekDayLunchHours}</div>
+                <div>Monday - Thursday / {props.weekendDinnerHours}</div>
+                <div>Friday & Saturday / {props.weekendDinnerHours}</div>
             </div>
         </div>
     );
